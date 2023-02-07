6f5d0b794eee151799d2073d067c7ec0073201f6
maish79 <maishsupa@yahoo.com>
2023-02-07 14:31:43 +0000

    added pagination

--- carts/views.py
+++ carts/views.py
@@ -60,6 +60,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -67,7 +69,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass  # just ignore
 
     context = {
