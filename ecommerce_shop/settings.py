0d91e1f9286799b1d09d207d79956bb9bc28e82d
maish79 <maishsupa@yahoo.com>
2023-02-09 13:19:46 +0000

    deployment part 2

--- ecommerce_shop/settings.py
+++ ecommerce_shop/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-dz9s@p(p!l+^9jjs0*kd%cb$yk8m*(atb7al!8_!hq(hyrzpt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-maina', 'localhost']
 
 
 # Application definition
@@ -83,12 +83,20 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+     }
     }
-}
+
+
+    
 
 
 # Password validation
