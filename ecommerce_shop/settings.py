011f513d72466b0869d5cc4d75d72b94834f49d5
maish79 <maishsupa@yahoo.com>
2023-02-09 19:51:02 +0000

    deployment commit

--- ecommerce_shop/settings.py
+++ ecommerce_shop/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'shop',
     'accounts',
     'store',
@@ -63,7 +66,7 @@ ROOT_URLCONF = 'ecommerce_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['TEMPLATES_DIR'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,13 +142,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    'ecommerce_shop/static',
-]
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=[os.path.join(BASE_DIR,static)]
+#STATIC_ROOT = BASE_DIR / 'static'
+#STATICFILES_DIRS = [
+#    'ecommerce_shop/static',
+#]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 
